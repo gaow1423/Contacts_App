2a810d05031a707b75387a1af08feb34712ef06e
Wei Gao <gaow1423@gmail.com>
2018-10-09 13:20:52 -0500

    Updates

---
+++ precache-manifest.b1448958f2018501b262f1ba243db1f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a66be40e553ff6bb88",
+    "url": "/Contacts.App/static/js/runtime~main.c7a66be4.js"
+  },
+  {
+    "revision": "192df18ce68175a4d9d9",
+    "url": "/Contacts.App/static/js/main.192df18c.chunk.js"
+  },
+  {
+    "revision": "d8ec8089e4f1f01529eb",
+    "url": "/Contacts.App/static/js/1.d8ec8089.chunk.js"
+  },
+  {
+    "revision": "192df18ce68175a4d9d9",
+    "url": "/Contacts.App/static/css/main.35319555.chunk.css"
+  },
+  {
+    "revision": "d8ec8089e4f1f01529eb",
+    "url": "/Contacts.App/static/css/1.71a2d823.chunk.css"
+  },
+  {
+    "revision": "e8d8be920cb233f1c348447824948a10",
+    "url": "/Contacts.App/index.html"
+  }
+];
