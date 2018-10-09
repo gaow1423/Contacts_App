6f653bdb58a09935b9a102974a8a447969e3af7d
Wei Gao <gaow1423@gmail.com>
2018-10-09 13:44:21 -0500

    ..

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -20,13 +20,9 @@ const Header = props => {
     // </div>
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <Link
-          href="/contact-manager"
-          className="navbar-brand"
-          style={headingStyle}
-        >
+        <a href="/" className="navbar-brand" style={headingStyle}>
           {branding}
-        </Link>
+        </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
