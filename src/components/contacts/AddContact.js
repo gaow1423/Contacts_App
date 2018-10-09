2391777d6bcd9da7099b494f7d7478da768c5625
Wei Gao <gaow1423@gmail.com>
2018-10-09 14:12:10 -0500

    ..

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import TextInputGroup from '../layout/TextInputGroup';
-import uuid from 'uuid';
 import axios from 'axios';
 
 class AddContact extends Component {
